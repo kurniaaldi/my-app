79327782a7e704c27e272155522c3dc7688f7159
aldi <aldi@enablr.id>
2022-06-04 16:21:43 +0700

    setup prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
 import Navbar from "components/atoms/nav-bar";
 
 function App() {
   return (
     <div className="w-full h-screen">
+      <CssBaseline />
       <Navbar />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
--- src/components/atoms/nav-bar/index.tsx
+++ src/components/atoms/nav-bar/index.tsx
@@ -1,67 +1,35 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 import React from "react";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
 export default function Navbar() {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   const [topScroll] = React.useState(true);
 
   return (
-    <nav
-      className={`flex-wrap items-center justify-between mobile:px-0 px-16 py-3 top-0 ${
-        topScroll ? "shadow-none" : "flex shadow"
-      } bg-white dark:bg-grey-dark-nav fixed w-full z-50`}
-    >
-      <div className="container mx-auto flex flex-wrap items-center justify-between">
-        <div className="w-full relative self-center flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-          <p>logo</p>
-          <button
-            className="flex items-center justify-between text-fontBlack cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent lg:hidden outline-none focus:outline-none"
-            type="button"
-            onClick={() => setNavbarOpen(!navbarOpen)}
-          ></button>
-          <ul className="lg:flex gap-2 flex-col lg:flex-row list-none lg:ml-auto sm:hidden mobile:hidden">
-            <li
-              onClick={() => setNavbarOpen(!navbarOpen)}
-              className="nav-item border-b border-primary dark:border-white"
-            >
-              <a className="text-primary py-2 flex items-start text-base leading-snug dark:text-white hover:text-primary">
-                <span className="ml-2">label</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div
-          className={`lg:flex flex-grow items-center
-              ${navbarOpen ? " flex" : "hidden"}`}
-          id="example-navbar-danger"
-        >
-          <ul className="flex gap-2 flex-col sm:items-start lg:items-center lg:flex-row list-none lg:ml-auto mobile:items-start">
-            <li
-              onClick={() => setNavbarOpen(!navbarOpen)}
-              className="nav-item border-b border-primary dark:border-white"
-            >
-              <a className="text-primary py-2 flex items-start text-base leading-snug dark:text-white hover:text-primary">
-                <span className="ml-2">label</span>
-              </a>
-            </li>
-            <li
-              onClick={() => setNavbarOpen(!navbarOpen)}
-              className="nav-item border-b border-primary dark:border-white"
-            >
-              <a className="text-primary py-2 flex items-start text-base leading-snug dark:text-white hover:text-primary">
-                <span className="ml-2">label</span>
-              </a>
-            </li>
-            <li
-              onClick={() => setNavbarOpen(!navbarOpen)}
-              className="nav-item border-b border-primary dark:border-white"
-            >
-              <a className="text-primary py-2 flex items-start text-base leading-snug dark:text-white hover:text-primary">
-                <span className="ml-2">label</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" className="bg-[#fff]">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
