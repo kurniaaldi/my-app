f3e508d5e73a986d957e8e8ca4b1c510819b870e
aldi <aldi@enablr.id>
2022-06-05 17:35:52 +0700

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
-import { Button, Grid, Typography } from "@mui/material";
+import { Chip, Typography } from "@mui/material";
 
-import { CODECLAZZ, MICROGEN, SIXTWOTRADE, STATS } from "assets/project";
+import { CODECLAZZ, MICROGEN, STATS } from "assets/project";
+import { GITHUB, GMAIL, LINKEDIN } from "assets/connect";
 import { Intro, Skills } from "components/molecules";
 import {
   KeenSliderInstance,
@@ -79,30 +80,56 @@ function App() {
       title: "Stats.enablr.id",
       thumb: <STATS className="w-24 h-auto" />,
       desc: "Stats adalah tools riset pemasaran yang membantu Anda menganalisa pasar, kompetisi dan trend produk untuk memaksimalkan aktivitas penjualan online di marketplace terbesar di Indonesia.",
+      stack: ["Next.Js", "Axios", "Tailwind", "formik", "etc."],
     },
     {
       image: "./images/62trade.png",
       title: "62Trade.com",
       thumb: <img src="./images/logo_name.png" className="w-30 h-auto" />,
-      desc: "Stats adalah tools riset pemasaran yang membantu Anda menganalisa pasar, kompetisi dan trend produk untuk memaksimalkan aktivitas penjualan online di marketplace terbesar di Indonesia.",
+      desc: "62Trade.com is ready to take important role to connect market from all around the world. We will take you to experience a simple way of importing and exporting your products. If you are thinking of selling and buying products, our platform can be a media to present the products that might suitable for your needs.",
+      stack: ["Next.Js", "Axios", "Scss", "formik", "etc."],
     },
     {
       image: "./images/codeclazz.png",
       title: "Codeclazz.com",
       thumb: <CODECLAZZ className="w-20 h-20" />,
-      desc: "Stats adalah tools riset pemasaran yang membantu Anda menganalisa pasar, kompetisi dan trend produk untuk memaksimalkan aktivitas penjualan online di marketplace terbesar di Indonesia.",
+      desc: "Platform Belajar Coding secara Hands On.",
+      stack: ["Next.Js", "Axios", "Apollo Graphql", "Material UI", "etc."],
     },
     {
       image: "./images/codeclazz.png",
       title: "Codeclazz Mobile",
       thumb: <CODECLAZZ className="w-20 h-20" />,
-      desc: "Stats adalah tools riset pemasaran yang membantu Anda menganalisa pasar, kompetisi dan trend produk untuk memaksimalkan aktivitas penjualan online di marketplace terbesar di Indonesia.",
+      desc: "Platform Belajar Coding secara Hands On.",
+      stack: [
+        "React Native",
+        "Apollo Graphql",
+        "React Native Paper",
+        "webview",
+        "etc",
+      ],
     },
     {
       image: "./images/microgen.png",
       title: "Microgen.com",
       thumb: <MICROGEN className="w-28 h-auto" />,
-      desc: "Stats adalah tools riset pemasaran yang membantu Anda menganalisa pasar, kompetisi dan trend produk untuk memaksimalkan aktivitas penjualan online di marketplace terbesar di Indonesia.",
+      desc: "Low Code Microservices Backend. Create Scalable App Faster!.",
+      stack: ["Next.Js", "Axios", "Apollo Graphql", "Material UI", "etc."],
+    },
+  ];
+
+  const connect = [
+    {
+      link: "aldikurniawan1606@gmail.com",
+      component: <GMAIL className="w-10 h-auto" />,
+    },
+    {
+      link: "https://www.linkedin.com/in/aldi-kurniawan/",
+      component: <LINKEDIN className="w-10 h-auto" />,
+    },
+    {
+      link: "https://github.com/kurniaaldi",
+      component: <GITHUB className="w-10 h-auto" />,
     },
   ];
 
@@ -135,13 +162,24 @@ function App() {
                           alt={item.title}
                           className="w-auto h-[19rem] object-contain rounded border border-primary"
                         />
-                        <div className="flex items-start justify-center gap-2 flex-col max-w-[20rem]">
+                        <div className="flex items-start justify-center gap-2 flex-col max-w-[25rem]">
                           <Typography
                             className="font-bold text-primary"
                             variant="h5"
                           >
                             {item.title}
                           </Typography>
+                          <div className="flex flex-wrap gap-2">
+                            {item.stack.map((stack: any) => {
+                              return (
+                                <Chip
+                                  key={stack}
+                                  label={stack}
+                                  className="text-text bg-background-light"
+                                />
+                              );
+                            })}
+                          </div>
 
                           <Typography className="text-[#fff]" paragraph>
                             {item.desc}
@@ -168,6 +206,25 @@ function App() {
             </div>
           </div>
         </main>
+        <footer className="w-full h-full min-h-[10rem] flex items-center justify-center flex-col gap-4">
+          <Typography className="font-bold text-primary" variant="h5">
+            Connect To Me
+          </Typography>
+          <div className="flex items-center justify-center gap-8">
+            {connect.map((item: any) => {
+              return (
+                <a
+                  key={item.link}
+                  href={item.link}
+                  target="_blank"
+                  rel="no noreferrer"
+                >
+                  {item.component}
+                </a>
+              );
+            })}
+          </div>
+        </footer>
       </div>
     </ThemeProvider>
   );
